b9d28ac9c60cb1af8bd566913374ecd2cf1d1cb2
pontsuyo <tsuyopon9221@gmail.com>
2021-01-24 13:46:13 +0900

    :tada: initial commit

---
+++ src/test/java/com/pontsuyo/tweet/analyzer/loader/TweetAnalyzerLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pontsuyo.tweet.analyzer.loader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetAnalyzerLoaderApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
