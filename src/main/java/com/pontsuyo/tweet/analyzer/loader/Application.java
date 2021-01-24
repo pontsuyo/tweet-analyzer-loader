8ba5419aa313a5cf1442e2744382fe12b5537cd0
pontsuyo <tsuyopon9221@gmail.com>
2021-01-24 21:14:05 +0900

    :+1: Applicationをcloud function仕様に変更

--- src/main/java/com/pontsuyo/tweet/analyzer/loader/Application.java
+++ src/main/java/com/pontsuyo/tweet/analyzer/loader/Application.java
@@ -1,27 +1,20 @@
 package com.pontsuyo.tweet.analyzer.loader;
 
+import java.util.function.Function;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class Application {
-  private static ConfigurableApplicationContext context = null;
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
 
-  public static <T> T getBean(Class<T> clazz){
-    return getContext().getBean(clazz);
-  }
-
-  private static ConfigurableApplicationContext getContext(){
-    if(context == null){
-      String args[] = new String[1];
-      args[0] = "dummy";
-      context = SpringApplication.run(Application.class, args);
-    }
-    return context;
+  @Bean
+  public Function<String, Boolean> containsCloud() {
+    return value -> value.contains("cloud");
   }
 }
