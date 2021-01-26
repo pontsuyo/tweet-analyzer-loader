9a44f8f4a149fc68868f2b48cc622fa260b60848
pontsuyo <tsuyopon9221@gmail.com>
2021-01-27 00:51:45 +0900

    :+1: ハンドラと関数の用意

--- src/main/java/com/pontsuyo/tweet/analyzer/loader/FunctionConfiguration.java
+++ src/main/java/com/pontsuyo/tweet/analyzer/loader/FunctionConfiguration.java
@@ -5,16 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-
 @SpringBootApplication
-public class Application {
+public class FunctionConfiguration {
 
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    SpringApplication.run(FunctionConfiguration.class, args);
   }
 
   @Bean
-  public Function<String, Boolean> containsCloud() {
-    return value -> value.contains("cloud");
+  public Function<String, String> uppercase() {
+    return value -> value.toUpperCase();
   }
 }
