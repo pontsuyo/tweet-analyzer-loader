6f5ff0cd1abf42d76d210e902984bff1c4014f43
pontsuyo <tsuyopon9221@gmail.com>
2021-02-06 09:28:30 +0900

    :+1: domain層調整

--- src/main/java/com/pontsuyo/tweet/analyzer/loader/domain/model/Tweet.java
+++ src/main/java/com/pontsuyo/tweet/analyzer/loader/domain/model/Tweet.java
@@ -1,18 +1,11 @@
 package com.pontsuyo.tweet.analyzer.loader.domain.model;
 
-import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import lombok.Getter;
+import lombok.Data;
 
-@Getter
-@Entity
-@Table(name = "tweet")
-public class Tweet implements Serializable {
+@Data
+public class Tweet {
 
-  @Id
   private Long id;
 
-  private String text;
+  private String text = "";
 }
