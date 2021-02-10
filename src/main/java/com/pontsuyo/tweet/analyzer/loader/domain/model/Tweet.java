310f100e88d15571bd721d6ec17b0a11180b89c5
pontsuyo <tsuyopon9221@gmail.com>
2021-02-10 23:07:09 +0900

    :recycle: モデルの整理とフィールドの追加

--- src/main/java/com/pontsuyo/tweet/analyzer/loader/domain/model/Tweet.java
+++ src/main/java/com/pontsuyo/tweet/analyzer/loader/domain/model/Tweet.java
@@ -1,11 +1,33 @@
 package com.pontsuyo.tweet.analyzer.loader.domain.model;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import lombok.Data;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 
 @Data
 public class Tweet {
 
-  private Long id;
+  private Long tweetId;
+
+  private Long userId;
 
   private String text = "";
+
+  private List<String> imageUrls = Collections.emptyList();
+
+  private Integer favoriteCount;
+
+  private Integer retweetCount;
+
+  public Map<String, AttributeValue> toQueryMap() {
+    return Map.of(
+        "id", AttributeValue.builder().n(tweetId.toString()).build(),
+        "text", AttributeValue.builder().s(text).build(),
+        "user_id", AttributeValue.builder().n(userId.toString()).build(),
+        "favorite_count", AttributeValue.builder().n(favoriteCount.toString()).build(),
+        "retweet_count", AttributeValue.builder().n(retweetCount.toString()).build()
+    );
+  }
 }
