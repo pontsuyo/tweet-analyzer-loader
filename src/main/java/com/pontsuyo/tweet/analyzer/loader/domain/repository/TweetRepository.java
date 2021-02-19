2d6a0b1595a17300aee2b3fd16b350d17a9dae1e
pontsuyo <tsuyopon9221@gmail.com>
2021-02-20 00:46:08 +0900

    :+1: separate repository interface

--- src/main/java/com/pontsuyo/tweet/analyzer/loader/domain/repository/TweetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pontsuyo.tweet.analyzer.loader.domain.repository;
-
-import com.pontsuyo.tweet.analyzer.loader.domain.model.Tweet;
-
-public interface TweetRepository {
-  void updateTweet(Tweet tweet);
-}
