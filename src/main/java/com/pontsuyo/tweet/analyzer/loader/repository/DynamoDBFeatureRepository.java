704d6338605c2043b0583403de2274422119833e
pontsuyo <tsuyopon9221@gmail.com>
2021-02-22 23:48:01 +0900

    :mute: remove unnecessary annotation

--- src/main/java/com/pontsuyo/tweet/analyzer/loader/repository/DynamoDBFeatureRepository.java
+++ src/main/java/com/pontsuyo/tweet/analyzer/loader/repository/DynamoDBFeatureRepository.java
@@ -2,12 +2,10 @@ package com.pontsuyo.tweet.analyzer.loader.repository;
 
 import com.pontsuyo.tweet.analyzer.loader.domain.model.TweetFeature;
 import com.pontsuyo.tweet.analyzer.loader.domain.repository.TweetFeatureRepository;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
 
-@Slf4j
 @Repository
 public class DynamoDBFeatureRepository implements TweetFeatureRepository {
 
